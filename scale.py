3f7678213555e51cb0dd30881bdb0094f873e3e4
Farhan Rahman <farhanrahman26@gmail.com>
2020-04-01 08:19:08 +0100

    Added padding when scaling the mask.

--- scale.py
+++ scale.py
@@ -16,15 +16,13 @@ class MeshScaler(object):
     around the mesh and getting the ratio of (reference length : bounding box length).
     '''
 
-    def __init__(self, mesh_path, mesh_obj = None):
+    def __init__(self, options, mesh_path):
+        self.options = options
         self.mesh_path = expanduser(mesh_path)
         if not os.path.exists(self.mesh_path):
             raise Exception("File not found at {}".format(self.mesh_path))
         self.mesh_name = os.path.basename(self.mesh_path)
-        if mesh_obj == None:
-            self.mesh_obj = mesh.Mesh.from_file(self.mesh_path)
-        else:
-            self.mesh_obj = mesh_obj
+        self.mesh_obj = mesh.Mesh.from_file(self.mesh_path)
 
     def getScaleFactor(self, ref_length):
         '''
@@ -39,7 +37,10 @@ class MeshScaler(object):
 
         dimensions = [(numpy.max(val) - numpy.min(val))
                         for val in [self.mesh_obj.x, self.mesh_obj.y, self.mesh_obj.z]]
+        padding_mm = (float(self.options.padding_in_cm) * 2 * 10.0)
         length_mm = max(dimensions)
+        assert(length_mm > padding_mm)
+        length_mm -= padding_mm
         scale_factor = (float(ref_length)*10)/length_mm
         return scale_factor
 
@@ -75,6 +76,10 @@ if __name__=="__main__":
 
     #Optionals
 
+    parser.add_option("--padding-in-cm", default=0.3,
+                      dest="padding_in_cm",
+                      help="Extra padding to be applied in the inside of the mask. 0.3 cm by default.")
+
     parser.add_option("--rest-files-regex", default=None,
                       dest="rest_files_regex",
                       help="Regex to capture other files to scale.")
@@ -91,17 +96,18 @@ if __name__=="__main__":
     if opt.nose_to_chin_in_cm == None:
         raise Exception("Please provide the option --nose-to-chin-in-cm.")
 
-    mask = MeshScaler(opt.mask_file)
+    mask = MeshScaler(opt, opt.mask_file)
     meshes = dict()
     meshes[mask.mesh_name] = mask
     scale_factor = mask.getScaleFactor(opt.nose_to_chin_in_cm)
+    print "scale_factor: {}".format(scale_factor)
 
     dir_name = os.path.dirname(expanduser(opt.mask_file))
     regex = re.compile(str(opt.rest_files_regex) if opt.rest_files_regex != None else ".*.stl")
     for filename in os.listdir(dir_name):
         if regex.search(filename):
             if filename not in meshes:
-                meshes[filename] = MeshScaler(dir_name + os.path.sep + filename)
+                meshes[filename] = MeshScaler(opt, dir_name + os.path.sep + filename)
 
     output_dir = None
     if opt.output_dir != None:
@@ -109,7 +115,8 @@ if __name__=="__main__":
     else:
         output_dir = "mesh_" + datetime.datetime.now().strftime("%d.%m.%Y.%H.%m.%S")
 
-    os.mkdir(output_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
     for m in meshes:
         meshes[m].writeOutScaled(output_dir, scale_factor)
